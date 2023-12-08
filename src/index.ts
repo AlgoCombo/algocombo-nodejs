2fb09d5cf96d7d724c9950f67afab13965037ded
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2023-12-09 01:00:29 +0530

    chore: comments

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
+import express from "express";
 
-import express from 'express'
+const app = express();
 
-const app = express()
+app.get("/addTrade", (req, res) => {
+  // Schedule trade, i.e. add to database
+});
 
-app.get('/swap')
+app.get("/execute", (req, res) => {
+    // Fetch all trades (Daily for now)
+    // Call Python API to run each trade's algorithm
+    // If positive, execute the trade using 1Inch
+    // Log the results in database
+})
