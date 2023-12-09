5576947b36c0913863062b21b0bbaa15673407e8
bamblebam <burhanizrangwala@gmail.com>
2023-12-09 20:45:26 +0530

    chore: additional params

--- src/models/trades.model.ts
+++ src/models/trades.model.ts
@@ -6,6 +6,10 @@ export interface ITrade extends Document {
   current_coin: string;
   coin_pairs: [string];
   amount: number;
+  chain_id: number;
+  order_details: string;
+  algorithm: string;
+  execution_type: string;
   creator: IUser;
   isActive: boolean;
   createdAt: Date;
@@ -18,6 +22,7 @@ export const TradeSchema: Schema = new mongoose.Schema<ITrade>({
   current_coin: String,
   coin_pairs: [String],
   amount: Number,
+  chain_id: Number,
   createdAt: {
     type: Date,
     default: Date.now, // Set the default value to the current date/time
@@ -27,6 +32,18 @@ export const TradeSchema: Schema = new mongoose.Schema<ITrade>({
     default: true,
   },
   creator: UserSchema,
+  algorithm: {
+    type: String,
+    default: "",
+  },
+  order_details: {
+    type: String,
+    default: "",
+  },
+  execution_type: {
+    type: String,
+    default: "fusionswap",
+  },
 });
 
 // Pre-save hook to generate trade_id before saving the document
