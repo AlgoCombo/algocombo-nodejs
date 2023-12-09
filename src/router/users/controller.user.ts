3770a2eb1497430cca4b1a78ea58c1416f901350
bamblebam <burhanizrangwala@gmail.com>
2023-12-10 03:20:45 +0530

    feat: create user happens when opening trade

--- src/router/users/controller.user.ts
+++ src/router/users/controller.user.ts
@@ -62,6 +62,44 @@ class UserController {
     }
   }
 
+  async createUserHelper(body: any) {
+    try {
+      const wallet_address = body.wallet_address;
+
+      const user = await UserModel.findOne({ wallet_address });
+      if (user) {
+        return {
+          status: 200,
+          message: "User already exists",
+          data: user.hot_wallet_public_key,
+        };
+      }
+
+      //create hot wallet
+      const hot_wallet_data = generateWallet();
+
+      const new_user_data = {
+        wallet_address,
+        hot_wallet_public_key: hot_wallet_data.address,
+        hot_wallet_private_key: hot_wallet_data.privateKey,
+      };
+      const new_user = await UserModel.create(new_user_data);
+
+      return {
+        status: 201,
+        message: "User created",
+        data: new_user,
+      };
+    } catch (error: any) {
+      console.log(error);
+      return {
+        status: 500,
+        message: "Internal server error",
+        data: error,
+      };
+    }
+  }
+
   async getHotWallet(body: any) {
     try {
       const wallet_address = body.wallet_address;
