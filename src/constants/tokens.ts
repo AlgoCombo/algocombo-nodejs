2225531cb498430d44a93fcfa0d4e161868559d7
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2023-12-09 19:52:34 +0530

    ft: polygon tokens list, install axios for swap

---
+++ src/constants/tokens.ts
@@ -0,0 +1,19 @@
+export const TOKENS = {
+  137: [
+    {
+      symbol: "WMATIC",
+      address: "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270",
+      decimals: 18,
+    },
+    {
+      symbol: "USDT",
+      address: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F",
+      decimals: 6,
+    },
+    {
+      symbol: "USDC",
+      address: "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359",
+      decimals: 6,
+    },
+  ],
+};
